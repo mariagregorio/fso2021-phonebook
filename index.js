2ffaf8ee990f60726a59655dc9563f7fa3626b57
maria gregorio <airam.greg@gmail.com>
2021-12-03 20:10:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+morgan.token('body', function (req) { return req.method === 'POST' ? JSON.stringify(req.body) : '' })
+
+app.use(morgan(
+  (tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.body(req, res)
+    ].join(' ')
+  })
+)
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+const generateRandomId = () => {
+  return Math.floor(Math.random() * 10000000)
+}
+
+app.get('/info', (req, res) => {
+  res.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>
+  `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id.toString() === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if(!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+
+  const person = persons.find(person => person.name.toUpperCase() === body.name.toUpperCase())
+
+  if(person) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const newPerson = {
+    id: generateRandomId(),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(newPerson)
+  res.json(newPerson)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id.toString() === id)
+  if(person) {
+    persons = persons.filter(p => p.id.toString() !== id)
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
